7d62df28e25639322c1b5572210a038de7243e69
Mario Rimann <mario@rimann.org>
2013-05-26 13:54:41 +0200

    [FEATURE] Install PHP 5.4 with FPM

--- manifests/default.pp
+++ manifests/default.pp
@@ -9,8 +9,24 @@ package { 'unzip':
 	ensure => present,
 }
 
+exec { "Import repo signing key to apt keys":
+	path   => "/usr/bin:/usr/sbin:/bin",
+	command     => "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E5267A6C",
+	unless      => "apt-key list | grep E5267A6C",
+}
+
+file { '/etc/apt/sources.list.d/php-5.4-repos.list':
+	ensure => present,
+	source => "/vagrant/manifests/files/php-5.4-repos.list",
+	notify => Service['php5-fpm'],
+}
+
 exec { 'apt-get update':
 	command => '/usr/bin/apt-get update',
+	require => [
+		File['/etc/apt/sources.list.d/php-5.4-repos.list'],
+		Exec["Import repo signing key to apt keys"],
+	],
 }
 
 
@@ -29,4 +45,56 @@ service { 'mysql':
 	hasrestart => true,
 	enable => true,
 	require => Package["mysql-server"],
+}
+
+# ---------------------------------------------------
+# Install PHP 5.4.x with FPM
+# ---------------------------------------------------
+
+package { 'php5-fpm':
+	ensure => installed,
+	require => Exec['apt-get update'],
+}
+
+package { 'libapache2-mod-fastcgi':
+	ensure => installed,
+	require => Exec['apt-get update'],
+	notify => Service['apache2'],
+}
+
+package { 'php5-mysql':
+	ensure => installed,
+	require => Exec['apt-get update'],
+	notify => Service['php5-fpm'],
+}
+package { 'php5-mcrypt':
+	ensure => installed,
+	require => Exec['apt-get update'],
+	notify => Service['php5-fpm'],
+}
+package { 'php5-curl':
+	ensure => installed,
+	require => Exec['apt-get update'],
+	notify => Service['php5-fpm'],
+}
+package { 'php5-gd':
+	ensure => installed,
+	require => Exec['apt-get update'],
+	notify => Service['php5-fpm'],
+}
+package { 'php5-cli':
+	ensure => installed,
+	require => Exec['apt-get update'],
+}
+package { 'php-apc':
+	ensure => installed,
+	require => Exec['apt-get update'],
+	notify => Service['php5-fpm'],
+}
+
+service { 'php5-fpm':
+	ensure => running,
+	require => Package['php5-fpm'],
+	hasrestart => true,
+	hasstatus => true,
 }
